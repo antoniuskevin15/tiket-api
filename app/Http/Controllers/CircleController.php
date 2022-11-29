a293dce49226b6618f89ca24b1549b5d319afbdc
Vallencius <vallencius1901@gmail.com>
2022-11-29 19:29:55 +0700

    Login

--- app/Http/Controllers/CircleController.php
+++ app/Http/Controllers/CircleController.php
@@ -2,9 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
 
 class CircleController extends Controller
 {
-    //
+    public function login(Request $request){
+        $user = User::where('email', $request->email)->get()->first();
+        if(!$user || Hash::check($request->passowrd, $user->password)){
+            return response()->json([
+                'message' => 'Invalid credentials!'
+            ], Response::HTTP_NOT_ACCEPTABLE);
+        }
+        $token = $user->createToken("loginToken")->plainTextToken;
+        return response()->json([
+            'user' => $user,
+            'loginToken' => $token
+        ], Response::HTTP_OK);
+    }
 }
