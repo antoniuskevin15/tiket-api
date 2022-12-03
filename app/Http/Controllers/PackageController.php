105d8a44fb183247b0bf46d9ac8035bae76cd798
Adrian Finantyo <adrianfinantyo@gmail.com>
2022-12-03 20:26:29 +0700

    fix: translate naming to enlish

--- app/Http/Controllers/PackageController.php
+++ app/Http/Controllers/PackageController.php
@@ -43,9 +43,9 @@ class PackageController extends Controller {
         $package = Package::create([
             'sender' => $request->sender,
             'expedition' => $request->expedition,
-            'receiptNumber' => $request->resi,
-            'roomNumber' => $request->nomorKamar,
-            'photoURL' => $image_path,
+            'receiptNumber' => $request->receiptNumber,
+            'roomNumber' => $request->roomNumber,
+            'photoPath' => $image_path,
             'user_id' => $request->user_id
         ]);
 
