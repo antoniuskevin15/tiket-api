7505ee508527c19587d09ab621ee1450b0a7f0a3
Adrian Finantyo <adrianfinantyo@gmail.com>
2022-12-15 23:07:36 +0700

    update: migration hosting

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -19,7 +19,10 @@ class Cors
         return $next($request)
             ->header('Access-Control-Allow-Origin', '*')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-            ->header('Access-Control-Allow-Headers', '*');
+            ->header('Access-Control-Allow-Headers', '*')
+            ->header('Access-Control-Allow-Credentials', 'true')
+            ->header('Access-Control-Max-Age', '86400')
+            ->header('Content-Type', '*');
 
     }
 }
